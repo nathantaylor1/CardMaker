a16df752afe5545bdc29de4f681c57136d40da0a
Nathan Taylor <67647086+nathantaylor1@users.noreply.github.com>
2022-06-10 21:39:39 -0400

    Tentatively Done...

--- cardmaker.py
+++ cardmaker.py
@@ -1,14 +1,19 @@
 # Card Maker for Jungle Defense by BarelyBetas
 # Code by Nathan Taylor (nathantaylor.nate@gmail.com)
+
+# Build using PyInstaller: 
+# python3 -m PyInstaller --onefile --noconsole cardmaker.py
+
 import PySimpleGUI as gui
 from textwrap import wrap
 from PIL import Image, ImageFont, ImageDraw
 
-def GenerateCard(name: str, cost: int, body: str):
+def GenerateCard(name: str, cost: int, body: str, filename: str):
     # Load Assets:
     card = Image.open('Assets/Card.png')
-    adventure_font = ImageFont.truetype('Assets/AdventureFont/Adventure.ttf', 52)
-    body_font = ImageFont.truetype('Assets/AdventureFont/Adventure.ttf', 38)
+    adventure_font = ImageFont.truetype('Assets/fonts/Adventure.ttf', 52)
+    cost_font = ImageFont.truetype('Assets/fonts/0xA000-Mono-Bold.ttf', 52)
+    body_font = ImageFont.truetype('Assets/fonts/Adventure.ttf', 38)
 
     # Card Picture
     #card_img = Image.open("Assets/Black.png")
@@ -22,7 +27,7 @@ def GenerateCard(name: str, cost: int, body: str):
     editable_card.text((60,45), name, (255, 255, 255), font=adventure_font)
 
     # Set Card Cost:
-    editable_card.text((450,45), cost, (255, 255, 255), font=adventure_font)
+    editable_card.text((450,45), cost, (0, 0, 0), font=cost_font)
 
     # Card Description:
     offset = 450
@@ -31,8 +36,7 @@ def GenerateCard(name: str, cost: int, body: str):
         offset += adventure_font.getsize(line)[1]
 
     # Save Result:
-    fname = name.replace(" ", "")
-    card.save(fname+'.png', "PNG")
+    card.save(filename+'.png', "PNG")
 
 if __name__ == "__main__":
     layout = [
@@ -41,8 +45,8 @@ if __name__ == "__main__":
             gui.Input(key="-NAME-")
         ],
         [
-            gui.Text("Card Cost: "),
-            gui.Input(key="-COST-")
+            gui.Text("Card Cost: (Leave Blank for 1-9)"),
+            gui.Input(key="-COST-", size=(5,1))
         ],
         #[
         #    gui.Text("Card Picture: "),
@@ -72,8 +76,15 @@ if __name__ == "__main__":
             card_name = values["-NAME-"]
             card_cost = values["-COST-"]
             card_desc = values["-DESC-"]
-            #card_img = values["-IMG-"]
-            GenerateCard(card_name, card_cost, card_desc)
-            break
+            #card_img = values["-IMG-"]fname = card_name.replace(" ", "")
+            if card_cost == "":
+                for i in range(1,10):
+                    str_i = str(i)
+                    fname = card_name.replace(" ", "")
+                    fname += str_i
+                    GenerateCard(card_name, str_i, card_desc, fname)
+            else:
+                fname = card_name.replace(" ", "")
+                GenerateCard(card_name, card_cost, card_desc, fname)
 
     window.close()
